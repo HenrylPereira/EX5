4fc4d011379c21bdcf01cf714b31af11c47a213b
Henry Lopes Pereira <73833452+HenrylPereira@users.noreply.github.com>
2022-11-03 23:45:06 -0300

    Add files via upload

--- untitled/src/main/java/org/example/janela/TarefasJanela.java
+++ untitled/src/main/java/org/example/janela/TarefasJanela.java
@@ -1,6 +1,7 @@
 package org.example.janela;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -10,9 +11,9 @@ public class TarefasJanela extends JFrame {
     JLabel label = new JLabel();
     JButton tarefaBtt = new JButton();
 
-    JCheckBox check = new JCheckBox("ata");
-    JCheckBox[] ata;
-    JList list = new JList();
+    JPanel panel2 = new JPanel();
+
+    JRadioButton radioButton = new JRadioButton("comprar leite");
 
     public TarefasJanela(){
         setTitle("Tarefas");
@@ -25,12 +26,23 @@ public class TarefasJanela extends JFrame {
     public void configurarJanela(){
         JPanel panel = new JPanel();
         label.setText("Caixa de entrada");
-        label.setPreferredSize( new Dimension(250, 80));
+        label.setPreferredSize( new Dimension(270, 80));
 
         tarefaBtt.setText("Nova Tarefa");
 
+
+
+        Border blackline;
+        blackline = BorderFactory.createLineBorder(Color.black);
+        panel2.setBorder(blackline);
+        panel2.setPreferredSize( new Dimension(300, 40));
+        radioButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        panel2.add(radioButton);
+
+
         panel.add(label);
         panel.add(tarefaBtt);
+        panel.add(panel2);
         add(panel);
     }
 }
